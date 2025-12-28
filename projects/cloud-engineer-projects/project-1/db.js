24a7ff71cb49d837ea2d526a2ad806cb03ed6598
ttudalor <terence.udalor@gmail.com>
2025-12-28 12:35:46 +0100

    second folder

---
+++ projects/cloud-engineer-projects/project-1/db.js
@@ -0,0 +1,15 @@
+// db.js
+const mysql = require("mysql2/promise");
+require("dotenv").config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "employee_user",
+  password: process.env.DB_PASSWORD || "StrongPassword123",
+  database: process.env.DB_NAME || "employee_db",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+module.exports = pool;
